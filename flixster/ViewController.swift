9e5f84db11c3e74c4def2d71e346ec8213de68f4
syeddayemi <41522492+syeddayemi@users.noreply.github.com>
2021-09-16 21:32:07 -0500

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Syed Dayemi on 9/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
